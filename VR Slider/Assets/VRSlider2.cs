3c3bb34c0151dff90ac9ab791304ccc4f0118a51
Oleg Frolov <frolololeg@gmail.com>
2022-02-24 00:39:03 +0300

    vrslidder 2

---
+++ VR Slider/Assets/VRSlider2.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRSlider2 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("entered");
+    }
+}
