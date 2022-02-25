d2ab0a52cfc83eedfaa1746f11f758708cecb76d
Oleg Frolov <frolololeg@gmail.com>
2022-02-26 00:11:16 +0300

    added a border and fixed movement glitches

--- VR Slider/Assets/Scripts/VRSlider.cs
+++ VR Slider/Assets/Scripts/VRSlider.cs
@@ -10,12 +10,18 @@ public class VRSlider : MonoBehaviour
 
     public UnityEvent onSliderIn;
     public UnityEvent onSliderOut;
+
+    [SerializeField] private TextMesh text;
     
     private Vector3 _snappedHandPosition;
     private Vector3 _snappedButtonDir;
     
     private Vector3 _handDirVec;
     private Transform _handTransform;
+
+    private float _tempOffset = 0;
+    private float _counterStep = 0.25f;
+    private int _counter = 0;
     
     
     
@@ -27,9 +33,7 @@ public class VRSlider : MonoBehaviour
             onSliderIn.Invoke();
             canBeInteracted = true;
             
-            
             _handTransform = other.transform;
-
             Transform buttonTransform = transform;
             _snappedButtonDir = buttonTransform.up;
         }
@@ -46,8 +50,27 @@ public class VRSlider : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _tempOffset = 0;
+            _snappedHandPosition = _handTransform.position;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canBeInteracted = false;
+            _tempOffset = 0;
+        }
+
+        if (!canBeInteracted)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, 5f * Time.deltaTime);
+        }
+        
         if (Input.GetKey(KeyCode.Space) && canBeInteracted)
         {
+            text.text = _counter.ToString();
+            
             _handDirVec = Helper.GetHandDirection(_snappedHandPosition, _handTransform.position);
             // Debug.DrawLine(_snappedHandPosition, _handTransform.position, Color.blue);
             
@@ -55,25 +78,30 @@ public class VRSlider : MonoBehaviour
             float offset = _handDirVec.magnitude * Mathf.Cos(angle);
             // Debug.DrawLine(_snappedHandPosition, _snappedHandPosition - transform.up * offset, Color.red);
 
+            if (offset > _tempOffset + _counterStep)
+            {
+                _counter--;
+                _tempOffset += _counterStep;
+            }
+            
+            if (offset < _tempOffset - _counterStep)
+            {
+                _counter++;
+                _tempOffset -= _counterStep;
+            }
+
+            print("temp offset: " + _tempOffset);
+            print("offset: " + offset);
+
             transform.localPosition = new Vector3(0, -offset, 0);
             // print("angle is " + angle);
-            print("yo");
+        }
+        else
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, 5f * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _snappedHandPosition = _handTransform.position;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            canBeInteracted = false;
-        }
-
-        if (!canBeInteracted)
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, 3f * Time.deltaTime);
-        }
+        
         // else
         // {
         //     
