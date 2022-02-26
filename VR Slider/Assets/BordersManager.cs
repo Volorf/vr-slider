feb2cbe273767090e5873bcf13d35b12a5070cdf
Oleg Frolov <frolololeg@gmail.com>
2022-02-26 20:30:02 +0300

    Implemented Up and Down methods for borders

--- VR Slider/Assets/BordersManager.cs
+++ VR Slider/Assets/BordersManager.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BordersManager : MonoBehaviour
 {
     public float step = 0.5f;
-    void Start()
+    private float _targetOffset = 0;
+    private float _targetLimit = 1.5f;
+    
+    [ContextMenu("Call Up()")]
+    public void Up()
     {
-        
+        if (_targetOffset >= _targetLimit) return;
+        _targetOffset += step;
+        transform.DOLocalMoveY(_targetOffset, 1f).SetEase(Ease.Linear);
+    }
+    
+    [ContextMenu("Call Down()")]
+    public void Down()
+    {
+        if (_targetOffset <= -_targetLimit) return;
+        _targetOffset -= step;
+        transform.DOLocalMoveY(_targetOffset, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Up();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Down();
+        }
     }
 }
