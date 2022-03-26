3e2b9af36510cc0966f517f9216454dfdefa3456
Oleg Frolov <frolololeg@gmail.com>
2022-03-26 19:47:49 +0400

    added skybox gradient shader

--- VR Slider/Assets/Scripts/VRSliderSettings.cs
+++ VR Slider/Assets/Scripts/VRSliderSettings.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName="Create VR Slider Settings")]
 public class VRSliderSettings : ScriptableObject
 {
+    [Header("Base")]
     public float step;
+    public int counter;
+    
+    [Header("Animation")]
     public float dur;
     public float snapSpeed;
     public int stepCountLimit;
     public float expendDur;
     public float collapseDur;
-    public int counter;
 }
